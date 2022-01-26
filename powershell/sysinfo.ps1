d7d2a339c302c5e7f26708fb65176baccb9c8a46
unknown <bellerkr>
2022-01-25 19:25:27 -0500

    powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,11 @@
+﻿function getIP{
+(Get-NetIPAddress).IPAddress | Select-String "192"
+}
+
+Write-Host(getIP)
+
+$IP = getIP
+
+Write-Host("This is our machine's IP: $IP")
+
+Write-Host("This machine's Ip is: {0}" -f $IP)
