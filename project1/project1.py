6c49f0b4bf4f95db8f8d33eef5545bf20fc382db
bellerkr <bellerkr@github.com>
2022-02-20 19:18:01 -0500

    project1

---
+++ project1/project1.py
@@ -0,0 +1,7 @@
+import datetime
+x = datetime.datetime.now()
+print("The date and time is currently: ")
+print(x.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+#code used as a reference from https://www.w3resource.com/python-exercises/python-basic-exercise-3.php
